0af402463e65a167f4a328ba3c43208483a4d8cd
MOUAD AGUIRAR <mouad.aguirar@alumni.univ-avignon.fr>
2021-05-28 01:58:27 +0200

    fixing favicon error and loading articles using api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path')
 const app = express();
-const PORT = 3001
+const PORT = 3001;
+const businessApi = require("./models/businessCategory");
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.disable('etag');
 
 app.get('/',(req, res) => {
+    businessApi.load();
     res.send("hello world");
 });
 
